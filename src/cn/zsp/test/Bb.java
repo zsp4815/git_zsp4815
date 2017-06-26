3cf32f5ae63dcc79ae81de0dace6c68e30fdf312
zsp4815 <775026319@qq.com>
2017-06-26 10:08:14 +0800

    第二次提交

--- src/cn/zsp/test/Bb.java
+++ src/cn/zsp/test/Bb.java
@@ -1,9 +1,8 @@
 package cn.zsp.test;
 
-public class HelloWord {
+public class Bb {
 
 	public static void main(String[] args) {
-		System.out.println("helloworld");
 		System.out.println("bb");
 	}
 
